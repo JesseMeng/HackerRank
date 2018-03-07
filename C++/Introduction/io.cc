45b22bec48e85aea54048df9234944950ace9e05
Jesse <jesselingxunmeng@gmail.com>
2018-03-07 12:20:52 -0500

    Create io.cc

---
+++ C++/Introduction/io.cc
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int a1,a2,a3;
+    cin >>a1>>a2>>a3;
+    cout <<a1+a2+a3;
+    return 0;
+}
